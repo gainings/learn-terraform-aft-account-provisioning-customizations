6ef6a5ae60b60eaf421a0656807626eb96860939
gainings <gain.beats@gmail.com>
2025-03-08 20:19:02 +0900

    test

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -11,3 +11,9 @@ resource "aws_iam_role_policy" "aft_states" {
     account_provisioning_customizations_sfn_arn = aws_sfn_state_machine.aft_account_provisioning_customizations.arn
   })
 }
+
+
+resource "aws_iam_role" "aft_gainings" {
+  name               = "aft-test-gainings-role"
+  assume_role_policy = templatefile("${path.module}/iam/trust-policies/states.tpl", { none = "none" })
+}
